f5ad29a47ff86589592cb039a078b821407894b8
Ramya-77 <93141010+Ramya-77@users.noreply.github.com>
2021-11-03 11:59:26 +0530

    Add files via upload

---
+++ hr and min convertions.c
@@ -0,0 +1,15 @@
+/* converting hours and minutes into minutes */
+
+#include<stdio.h>
+void main()
+{
+	int hr, m, min;
+	
+	hr = 5;
+	m = 45;
+	min = (hr*60)+m;
+	
+	printf("%d minutes", min);
+	
+	
+}
