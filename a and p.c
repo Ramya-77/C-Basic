2d720ace03cb250362cf511e9455c29ceb0b0a29
Ramya-77 <93141010+Ramya-77@users.noreply.github.com>
2021-11-03 11:28:45 +0530

    Add files via upload

---
+++ a and p.c
@@ -0,0 +1,17 @@
+/* area and perimeter of the square */
+
+#include<stdio.h>
+void main()
+{
+	int s,a,p;
+	
+	s = 7;
+	
+	a = s*s;
+	p = 4*s;
+	
+	printf("area of the square with side %d is %d\n", s, a);
+	printf("perimeter of the square with side %d is %d", s, p);
+	
+	
+}
