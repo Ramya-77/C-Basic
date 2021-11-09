cb3ae8f071030555c06408db6b56207564642253
Ramya-77 <93141010+Ramya-77@users.noreply.github.com>
2021-11-09 12:20:51 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter a number (65 to 90) or (97 to 122)");
+	scanf("%d", &a);
+	
+	
+	printf("Corresponding ASCII character for %d is %c", a, a);
+	
+}
