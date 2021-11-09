b7fb8913aa38cf1eaf7907580186e11fffd45da0
Ramya-77 <93141010+Ramya-77@users.noreply.github.com>
2021-11-09 12:06:38 +0530

    Add files via upload

---
+++ scanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	
+	int p,q,r,s;
+	printf("enter all numbers");
+	scanf("%d%d%d%d", &p, &q, &r, &s);
+	
+	printf("first number: %d\n", p);
+	printf("second number: %d\n", q);
+	printf("third number: %d\n", r);
+	printf("fourth number: %d\n", s);
+}
