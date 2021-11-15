1e3a483eaf263794f47cf299684e3b24be53cd72
Ramya-77 <93141010+Ramya-77@users.noreply.github.com>
2021-11-15 12:12:19 +0530

    Delete decode.c

--- decode.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main()
-{
-	int a,b,c,d,e;
-	printf("enter five numbers");
-	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
-	printf("%c%c%c%c%c", a+95, b+95, c+95, d+95, e+95);	
-}
